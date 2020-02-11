6956d0035a8f3f7c32dd24fe13301ebda591d40f
shseo@digicaps.com <shseo@digicaps.com>
2020-02-11 11:03:57 +0900

    [update] add icon in Appbar. change title font in Appbar.

--- app/src/main/java/io/animal/monkey/MainActivity.java
+++ app/src/main/java/io/animal/monkey/MainActivity.java
@@ -1,12 +1,20 @@
 package io.animal.monkey;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.drm.DrmStore;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
+import android.util.Log;
 
 import io.animal.monkey.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,25 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
+        try {
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                // app icon
+                actionBar.setIcon(R.drawable.ic_child_care_black_24dp);
+                actionBar.setDisplayUseLogoEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+
+                // Rubik font
+                SpannableString s = new SpannableString(" BABYLOCK");
+                s.setSpan(new TypefaceSpan("rubik_black.ttf"), 0, s.length(),
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                actionBar.setTitle(s);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
+
 }
